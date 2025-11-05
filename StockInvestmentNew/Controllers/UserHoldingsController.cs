076962cbeb4aa75883cffaca8d062a318116a233
Manuraj <manu2raj@yahoo.co.in>
2025-11-05 13:14:26 +0530

    Add controller

---
+++ StockInvestmentNew/Controllers/UserHoldingsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockInvestmentNew.Models;
+
+namespace StockInvestmentNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserHoldingsController : ControllerBase
+    {
+        private readonly StockMarketContext _context;
+
+        public UserHoldingsController(StockMarketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserHoldings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserHolding>>> GetUserHoldings()
+        {
+            return await _context.UserHoldings.ToListAsync();
+        }
+
+        // GET: api/UserHoldings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserHolding>> GetUserHolding(int id)
+        {
+            var userHolding = await _context.UserHoldings.FindAsync(id);
+
+            if (userHolding == null)
+            {
+                return NotFound();
+            }
+
+            return userHolding;
+        }
+
+        // PUT: api/UserHoldings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserHolding(int id, UserHolding userHolding)
+        {
+            if (id != userHolding.HoldingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userHolding).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserHoldingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserHoldings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserHolding>> PostUserHolding(UserHolding userHolding)
+        {
+            _context.UserHoldings.Add(userHolding);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserHolding", new { id = userHolding.HoldingId }, userHolding);
+        }
+
+        // DELETE: api/UserHoldings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserHolding(int id)
+        {
+            var userHolding = await _context.UserHoldings.FindAsync(id);
+            if (userHolding == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserHoldings.Remove(userHolding);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserHoldingExists(int id)
+        {
+            return _context.UserHoldings.Any(e => e.HoldingId == id);
+        }
+    }
+}
---
+++ StockInvestmentNew/Controllers/WatchlistsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockInvestmentNew.Models;
+
+namespace StockInvestmentNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WatchlistsController : ControllerBase
+    {
+        private readonly StockMarketContext _context;
+
+        public WatchlistsController(StockMarketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Watchlists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Watchlist>>> GetWatchlists()
+        {
+            return await _context.Watchlists.ToListAsync();
+        }
+
+        // GET: api/Watchlists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Watchlist>> GetWatchlist(int id)
+        {
+            var watchlist = await _context.Watchlists.FindAsync(id);
+
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            return watchlist;
+        }
+
+        // PUT: api/Watchlists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWatchlist(int id, Watchlist watchlist)
+        {
+            if (id != watchlist.WatchlistId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(watchlist).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WatchlistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Watchlists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Watchlist>> PostWatchlist(Watchlist watchlist)
+        {
+            _context.Watchlists.Add(watchlist);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWatchlist", new { id = watchlist.WatchlistId }, watchlist);
+        }
+
+        // DELETE: api/Watchlists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWatchlist(int id)
+        {
+            var watchlist = await _context.Watchlists.FindAsync(id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Watchlists.Remove(watchlist);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WatchlistExists(int id)
+        {
+            return _context.Watchlists.Any(e => e.WatchlistId == id);
+        }
+    }
+}
